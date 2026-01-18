4fa63f7974240563bb664e8e51040fd4728757b6
MohamedAliBouzir <bzr.medali@gmail.com>
2026-01-18 12:52:30 +0100

    fixed some layout detected issues

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Container, Link, Divider, IconButton, useTheme, useMediaQuery } from '@mui/material';
+import { Box, Typography, Container, Link, IconButton, useTheme, useMediaQuery } from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -9,20 +9,7 @@ const Footer = ({ detail = false }: { detail?: boolean }) => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-    const footerSections = [
-        {
-            title: 'Company',
-            links: ['About', 'Careers', 'Mobile', 'Blog', 'How we work']
-        },
-        {
-            title: 'Contact',
-            links: ['Help/FAQ', 'Press', 'Affiliates', 'Hotel owners', 'Partners', 'Advertise with us']
-        },
-        {
-            title: 'More',
-            links: ['Airline fees', 'Airlines', 'Low fare tips', 'Badges & Certificates', 'Security']
-        }
-    ];
+
 
     if (!detail) {
         return (
@@ -50,40 +37,6 @@ const Footer = ({ detail = false }: { detail?: boolean }) => {
     return (
         <Box sx={footerStyles.root(theme, true)}>
             <Container maxWidth="lg">
-                <Box sx={{ display: 'grid', gridTemplateColumns: { xs: '1fr 1fr', sm: '1fr 1fr 1fr 1fr' }, gap: 4, mb: 6 }}>
-                    {footerSections.map((section) => (
-                        <Box key={section.title}>
-                            <Typography variant="subtitle1" fontWeight="bold" gutterBottom>
-                                {section.title}
-                            </Typography>
-                            <Box sx={footerStyles.gridItem}>
-                                {section.links.map((link) => (
-                                    <Link
-                                        key={link}
-                                        href="#"
-                                        underline="hover"
-                                        color="text.secondary"
-                                        sx={footerStyles.link}
-                                    >
-                                        {link}
-                                    </Link>
-                                ))}
-                            </Box>
-                        </Box>
-                    ))}
-                    <Box>
-                        <Typography variant="subtitle1" fontWeight="bold" gutterBottom>
-                            Get the Spotter app
-                        </Typography>
-                        <Box sx={footerStyles.appLinkBox}>
-                            <Box component="img" sx={footerStyles.appBadge} src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" />
-                            <Box component="img" sx={footerStyles.appBadge} src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg" />
-                        </Box>
-                    </Box>
-                </Box>
-
-                <Divider sx={{ mb: 4 }} />
-
                 <Box sx={footerStyles.bottomBar(isMobile)}>
                     <Box sx={footerStyles.legalLinks(isMobile)}>
                         <Typography variant="caption" color="text.secondary">© {new Date().getFullYear()} SPOTTER</Typography>
@@ -116,6 +69,10 @@ const Footer = ({ detail = false }: { detail?: boolean }) => {
                                 {route.label}
                             </Link>
                         ))}
+                    </Box>
+                    <Box sx={{ display: 'flex', gap: 2 }}>
+                        <Box component="img" sx={footerStyles.appBadge} src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" />
+                        <Box component="img" sx={footerStyles.appBadge} src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg" />
                     </Box>
                 </Box>
             </Container>
